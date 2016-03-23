c006f79e1121ebaa2dc9b71a6a9d78be12bdb783
alexeigo <alex.golyashov@rangle.io>
2016-03-23 17:56:16 -0400

    Update login.steps.js
    moved hardcoded values into reusable variables.

--- features/step-definitions/login.steps.js
+++ features/step-definitions/login.steps.js
@@ -4,29 +4,34 @@ const expect = require('chai').expect;
 const APP_URL = 'http://localhost:8080/';
 const TIMEOUT_MS = 1000;
 const PAUSE_MS = 1000;
+const USERNAME_INPUT = '#qa-uname-input';
+const PASSWORD_INPUT = '#qa-password-input';
+const LOGIN_BUTTON = '#qa-login-button';
+const COUNTER_PAGE = '#qa-counter';
+const ALERT_FIELD = '#qa-alert';
 
 module.exports = function() {
 
   this.Given(/^I'm looking at the login form$/, function() {
     return browser.url(APP_URL)
-      .then(() => browser.waitForVisible('#qa-uname-input', TIMEOUT_MS))
+      .then(() => browser.waitForVisible(USERNAME_INPUT, TIMEOUT_MS))
   });
 
   this.When(/^I log in with "([^"]*)" and "([^"]*)"$/, function(user, pass) {
-    return browser.click('#qa-uname-input')
+    return browser.click(USERNAME_INPUT)
       .then(() => browser.keys(user))
-      .then(() => browser.click('#qa-password-input'))
+      .then(() => browser.click(PASSWORD_INPUT))
       .then(() => browser.keys(pass))
       //.then(() => browser.pause(PAUSE_MS))
-      .then(() => browser.click('#qa-login-button'));
+      .then(() => browser.click(LOGIN_BUTTON));
   });
 
   this.Then(/^I should see the counter page$/, function() {
-    return browser.waitForVisible('#qa-counter', TIMEOUT_MS)
-      .then(() => browser.click('#qa-logout-button'));
+    return browser.waitForVisible(COUNTER_PAGE, TIMEOUT_MS)
+      .then(() => browser.click(LOGIN_BUTTON));
   });
 
   this.Then(/^I should see the credential warning/, function () {
-    return browser.waitForVisible('#qa-alert', TIMEOUT_MS);
+    return browser.waitForVisible(ALERT_FIELD, TIMEOUT_MS);
   });
 };
